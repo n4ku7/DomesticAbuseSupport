c4afc44bedc8aaf7d8d654e34dde29f89be4e382
n4ku7 <thenakulojha@gmail.com>
2026-02-25 10:43:31 +0530

    Added root redirect to login

--- dv_support/urls.py
+++ dv_support/urls.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
+
+def home_redirect(request):
+    return redirect('login')
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('core.urls')),
+    path('', home_redirect),
     path('', include('accounts.urls')),
+    path('', include('core.urls')),
     path('', include('support.urls')),
-    path('', include('communication.urls')),
+    path('admin/', admin.site.urls),
 ]
